da6b058515eab4b3712faf14321fa8d0d2f2d016
Josue Acevedo <acevedosuarezjosue@gmail.com>
2022-05-26 18:58:54 -0500

    que me amonesten es mi pasión

---
+++ js/login.js
@@ -0,0 +1,22 @@
+const btn = document.getElementById('btn-iniciar');
+let correo = "correo";
+let pass = "pass"
+
+btn.onclick = () =>{
+    correo = document.getElementById('correo').value;
+    pass = document.getElementById('password').value;
+    if(correo == "admin" && pass == "admin"){
+        window.location.href = 'home.html'
+    }else{
+        alert("Datos inválidos, reinténtalo por favor");
+    }
+    
+
+}
+
+const btnnew = document.getElementById('btnnew');
+
+btnnew.onclick = () =>{
+  window.location.href = 'registro.html'
+}
+
---
+++ js/registro.js
@@ -0,0 +1,5 @@
+  const btnCrear = document.getElementById('btn-crear');
+  
+  btnCrear.onclick = () =>{
+      window.location.href = 'home.html'
+  }
